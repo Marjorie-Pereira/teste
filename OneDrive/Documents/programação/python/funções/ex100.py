07d825c8c3ee524b5e01af42501c65944ee3e25c
Marjorie-Pereira <marjorie300904@gmail.com>
2023-06-30 15:40:55 -0300

    this is the message

---
+++ OneDrive/Documents/programação/python/funções/ex100.py
@@ -0,0 +1,25 @@
+lista = [1, 11, 3, 4]
+print(lista)
+
+lista.pop(2) # o numero entre parenteses representa a posição do elemento na lista
+print(lista)
+
+lista.remove(1) # o numero entre parenteses representa o número em si que será removido da lista
+print(lista)
+
+lista.append(10)
+print(lista)
+
+lista.sort() # ordem crescente
+print(lista)
+
+lista.sort(reverse=True) # ordem decrescente
+print(lista)
+
+print(lista[2])
+
+listaNova = [10, 90, 34]
+lista.append(listaNova)
+print(lista)
+
+zkjdkasjdkj
